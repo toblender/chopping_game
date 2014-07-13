21489eabc0b2354df227bc3d718aeb63aa3b625f
Danny Hui <danny@defyent.com>
2014-07-12 23:18:07 -0400

    Working chopping app.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,50 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('GameBoardCtrl', ['$scope', function($scope) {
+    //Generate the current gameboard
+  
+    var maxCells = 6; 
+    $scope.rows=[];
+    $scope.counter=0;
+    $scope.playerSide=0;
 
+    function generateBranch(currentRow){
+        var side = Math.round(Math.random()); //0 left, 1 right
+        //Make branches appear 70% of the time
+        var hasBranch = !(Math.random()>=0.7 && Math.random()<=1);
+        return {side:side,
+                hasBranch:hasBranch};
+    }
+
+    function createNextBranch(){
+        var cells={};
+        if ($scope.counter % 2){
+            cells = generateBranch();
+        }else{
+            cells = {side:0,hasBranch:0};
+        }
+        $scope.rows.unshift(cells);
+        if ($scope.rows.length > maxCells){
+            $scope.rows.pop();
+        } 
+        $scope.counter+=1;
+    }
+
+    function generateGameBoard(){
+        for (var i=0;i<maxCells;i++){
+            createNextBranch();
+        }
+        console.log($scope.rows);
+    }
+    generateGameBoard();
+
+    $scope.chop=function(side){
+        $scope.playerSide=side;
+        createNextBranch();
+        var currentLevel = $scope.rows[$scope.rows.length-1];
+        if (currentLevel.side == side && currentLevel.hasBranch){
+            alert('You dead.');
+        }
+    }
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
